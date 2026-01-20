000b57d0b4da294db8f51f5614d422a6376ac36c
Matheus Moreira <matheusgmoreira1@gmail.com>
2026-01-20 13:26:42 -0300

    estudos sobre arrays

---
+++ aula13/index.js
@@ -0,0 +1,41 @@
+//            0123456789101112
+//const nome = 'Matheus Oliveira'
+//console.log(nome[10]);
+
+//                  0        1          2
+let alunos = ['Matheus', 'Maria', 'João'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+//console.log(alunos.slice(0, -2));
+
+//console.log(alunos[50]);
+
+//delete alunos[1];
+//console.log(alunos[1]);
+
+//const removido = alunos.shift(); //
+//const removido = alunos.pop(); //
+//console.log(removido);
+
+//alunos.unshift('Luiza'); // Adiciona no começo
+//alunos.unshift('Fabio');
+
+//alunos.push('Lucas'); // Adiciona no fim
+
+// alunos[alunos.length] = 'Luiza':
+// alunos[alunos.length] =  'Fabio'; // Adiciona no fim
+// aluno[alunos.length] = 'Luana';
+
+//alunos[0] = 'Caio'; // Altera
+//alunos[3] = 'Luiza';
+
+//console.log(alunos);
+//console.log(alunos[0]); // Adicionando
+//console.log(alunos[2]);
+
