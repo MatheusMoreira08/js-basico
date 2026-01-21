4a347b5e8a766e07d0324e93de6420a569d8fdd8
Matheus Moreira <matheusgmoreira1@gmail.com>
2026-01-21 13:22:44 -0300

    funcoes

---
+++ aula14/index.js
@@ -0,0 +1,28 @@
+//function saudacao(nome) {
+//    return `Bom Dia ${nome}!`;
+//}
+
+//const variavel = saudacao('Luiz');
+//console.log(variavel);
+
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+//const resultado = soma();
+//console.log(resultado);
+
+
+//console.log(soma(2, 3));
+//console.log(soma(3, 1));
+//console.log(soma(5, 10));
+
+const raiz = function (n) {  // <- mesma coisa ->   // const raiz = n => n ** 0.5
+    return n ** 0.5;
+}
+
+console.log(raiz(9));
+console.log(raiz(16));
+
+
